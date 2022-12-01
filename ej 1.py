3a424dd50b30c25b73409617ce78831e88c6fde8
ireneangulo <113629834+ireneangulo@users.noreply.github.com>
2022-12-01 21:30:31 +0000

    ej1def

--- ej 1.py
+++ ej 1.py
@@ -1,180 +1,180 @@
 """
-Implementación de un árbol de decisión de regresión básico.
-Conjunto de datos de entrada: el conjunto de datos de entrada debe ser unidimensional con etiquetas continuas.
-Salida: el árbol de decisiones asigna una entrada de número real a una salida de número real.
+Implementation of a basic regression decision tree.
+Input data set: The input data set must be 1-dimensional with continuous labels.
+Output: The decision tree maps a real number input to a real number output.
 """
-importar  numpy  como  np
+import numpy as np
 
 
- árbol de decisión de clase :
-    def  __init__ ( self , profundidad = 5 , min_leaf_size = 5 ):
-        uno mismo profundidad  =  profundidad
-        uno mismo límite_decisión  =  0
-        uno mismo izquierda  =  Ninguno
-        uno mismo derecho  =  ninguno
-        uno mismo min_leaf_size  =  min_leaf_size
-        uno mismo predicción  =  Ninguno
+class DecisionTree:
+    def __init__(self, depth=5, min_leaf_size=5):
+        self.depth = depth
+        self.decision_boundary = 0
+        self.left = None
+        self.right = None
+        self.min_leaf_size = min_leaf_size
+        self.prediction = None
 
-    def  mean_squared_error ( self , etiquetas , predicción ):
+    def mean_squared_error(self, labels, prediction):
         """
-        error medio cuadrado:
-        Etiquetas @param: una matriz numpy unidimensional
-        @param predicción: un valor de coma flotante
-        valor de retorno: mean_squared_error calcula el error si se utiliza la predicción para
-            estimar las etiquetas
-        >>> probador = Árbol de decisión()
-        >>> etiquetas_prueba = np.matriz([1,2,3,4,5,6,7,8,9,10])
-        >>> prueba_prediccion = float(6)
+        mean_squared_error:
+        @param labels: a one dimensional numpy array
+        @param prediction: a floating point value
+        return value: mean_squared_error calculates the error if prediction is used to
+            estimate the labels
+        >>> tester = DecisionTree()
+        >>> test_labels = np.array([1,2,3,4,5,6,7,8,9,10])
+        >>> test_prediction = float(6)
         >>> tester.mean_squared_error(test_labels, test_prediction) == (
-        ... TestDecisionTree.helper_mean_squared_error_test(test_labels,
-        ... prueba_predicción))
-        Verdadero
-        >>> etiquetas_prueba = np.matriz([1,2,3])
-        >>> prueba_prediccion = float(2)
+        ...     TestDecisionTree.helper_mean_squared_error_test(test_labels,
+        ...         test_prediction))
+        True
+        >>> test_labels = np.array([1,2,3])
+        >>> test_prediction = float(2)
         >>> tester.mean_squared_error(test_labels, test_prediction) == (
-        ... TestDecisionTree.helper_mean_squared_error_test(test_labels,
-        ... prueba_predicción))
-        Verdadero
+        ...     TestDecisionTree.helper_mean_squared_error_test(test_labels,
+        ...         test_prediction))
+        True
         """
-        si  etiquetas . ndim  !=  1 :
-            print ( "Error: las etiquetas de entrada deben ser unidimensionales" )
+        if labels.ndim != 1:
+            print("Error: Input labels must be one dimensional")
 
-        volver  np . media (( etiquetas  -  predicción ) **  2 )
+        return np.mean((labels - prediction) ** 2)
 
-    def  entrenar ( self , x , y ):
+    def train(self, x, y):
         """
-        tren:
-        @param x: una matriz numpy unidimensional
-        @param y: una matriz numpy unidimensional.
-        Los contenidos de y son las etiquetas para los valores correspondientes de X
-        el tren no tiene un valor de retorno
+        train:
+        @param x: a one dimensional numpy array
+        @param y: a one dimensional numpy array.
+        The contents of y are the labels for the corresponding X values
+        train does not have a return value
         """
-
-        """
-        esta sección es para verificar que las entradas se ajusten a nuestra dimensionalidad
-        restricciones
-        """
-        si  x _ ndim  !=  1 :
-            print ( "Error: el conjunto de datos de entrada debe ser unidimensional" )
-            devolver
-        si  largo ( x ) !=  largo ( y ):
-            print ( "Error: X e y tienen longitudes diferentes" )
-            devolver
-        si  y . ndim  !=  1 :
-            print ( "Error: las etiquetas del conjunto de datos deben ser unidimensionales" )
-            devolver
-
-        si  len ( x ) <  2  *  self . min_leaf_size :
-            uno mismo predicción  =  np . media ( y )
-            devolver
-
-        si  uno mismo . profundidad  ==  1 :
-            uno mismo predicción  =  np . media ( y )
-            devolver
-
-        mejor_división  =  0
-        min_error  =  uno mismo . error_cuadrado_medio ( x , np . medio ( y )) *  2
 
         """
-        bucle sobre todas las divisiones posibles para el árbol de decisión. encontrar la mejor división.
-        si no existe una división, es menos de 2 * error para toda la matriz
-        entonces el conjunto de datos no se divide y el promedio de toda la matriz se usa como
-        el predictor
+        this section is to check that the inputs conform to our dimensionality
+        constraints
         """
-        para  i  en  rango ( len ( x )):
-            si  len ( x [: i ]) <  self . min_leaf_size :
-                Seguir
-            elif  len ( x [ i :]) <  self . min_leaf_size :
-                Seguir
-            más :
-                error_left  =  uno mismo . error_cuadrado_medio ( x [: i ], np . media ( y [: i ]))
-                error_right  =  self . error_cuadrado_medio ( x [ i :], np . media ( y [ i :]))
-                error  =  error_izquierda  +  error_derecha
-                si  error  <  min_error :
-                    mejor_split  =  yo
-                    min_error  =  error
+        if x.ndim != 1:
+            print("Error: Input data set must be one dimensional")
+            return
+        if len(x) != len(y):
+            print("Error: X and y have different lengths")
+            return
+        if y.ndim != 1:
+            print("Error: Data set labels must be one dimensional")
+            return
 
-        si  mejor_división  !=  0 :
-            izquierda_x  =  x [: mejor_división ]
-            izquierda_y  =  y [: mejor_división ]
-            right_x  =  x [ mejor_división :]
-            right_y  =  y [ best_split :]
+        if len(x) < 2 * self.min_leaf_size:
+            self.prediction = np.mean(y)
+            return
 
-            uno mismo límite_decisión  =  x [ mejor_división ]
-            uno mismo izquierda  = árbol de  decisión (
-                profundidad = uno mismo . profundidad  -  1 , min_leaf_size = self . min_leaf_size
+        if self.depth == 1:
+            self.prediction = np.mean(y)
+            return
+
+        best_split = 0
+        min_error = self.mean_squared_error(x, np.mean(y)) * 2
+
+        """
+        loop over all possible splits for the decision tree. find the best split.
+        if no split exists that is less than 2 * error for the entire array
+        then the data set is not split and the average for the entire array is used as
+        the predictor
+        """
+        for i in range(len(x)):
+            if len(x[:i]) < self.min_leaf_size:
+                continue
+            elif len(x[i:]) < self.min_leaf_size:
+                continue
+            else:
+                error_left = self.mean_squared_error(x[:i], np.mean(y[:i]))
+                error_right = self.mean_squared_error(x[i:], np.mean(y[i:]))
+                error = error_left + error_right
+                if error < min_error:
+                    best_split = i
+                    min_error = error
+
+        if best_split != 0:
+            left_x = x[:best_split]
+            left_y = y[:best_split]
+            right_x = x[best_split:]
+            right_y = y[best_split:]
+
+            self.decision_boundary = x[best_split]
+            self.left = DecisionTree(
+                depth=self.depth - 1, min_leaf_size=self.min_leaf_size
             )
-            uno mismo derecha  = árbol de  decisión (
-                profundidad = uno mismo . profundidad  -  1 , min_leaf_size = self . min_leaf_size
+            self.right = DecisionTree(
+                depth=self.depth - 1, min_leaf_size=self.min_leaf_size
             )
-            uno mismo izquierda _ tren ( izquierda_x , izquierda_y )
-            uno mismo correcto _ tren ( derecha_x , derecha_y )
-        más :
-            uno mismo predicción  =  np . media ( y )
+            self.left.train(left_x, left_y)
+            self.right.train(right_x, right_y)
+        else:
+            self.prediction = np.mean(y)
 
-        devolver
+        return
 
-    def  predecir ( self , x ):
+    def predict(self, x):
         """
-        predecir:
-        @param x: un valor de punto flotante para predecir la etiqueta de
-        la función de predicción funciona llamando recursivamente a la función de predicción
-        de los subárboles apropiados en función del límite de decisión del árbol
+        predict:
+        @param x: a floating point value to predict the label of
+        the prediction function works by recursively calling the predict function
+        of the appropriate subtrees based on the tree's decision boundary
         """
-        si  uno mismo . la predicción  no es  ninguna : 
-            devolverse  a uno mismo . predicción
-        elif  auto . izquierda  o  uno mismo . el derecho  no es  ninguno : 
-            si  x  >=  uno mismo . límite_decisión :
-                devolverse  a uno mismo . correcto _ predecir ( x )
-            más :
-                devolverse  a uno mismo . izquierda _ predecir ( x )
-        más :
-            print ( "Error: árbol de decisión aún no entrenado" )
-            volver  Ninguno
+        if self.prediction is not None:
+            return self.prediction
+        elif self.left or self.right is not None:
+            if x >= self.decision_boundary:
+                return self.right.predict(x)
+            else:
+                return self.left.predict(x)
+        else:
+            print("Error: Decision tree not yet trained")
+            return None
 
 
-clase  TestDecisionTree :
-    """Decisión Clase de prueba Tres"""
+class TestDecisionTree:
+    """Decision Tres test class"""
 
-    @ método estático
-    def  helper_mean_squared_error_test ( etiquetas , predicción ):
+    @staticmethod
+    def helper_mean_squared_error_test(labels, prediction):
         """
         helper_mean_squared_error_test:
-        Etiquetas @param: una matriz numpy unidimensional
-        @param predicción: un valor de coma flotante
-        valor de retorno: helper_mean_squared_error_test calcula el error cuadrático medio
+        @param labels: a one dimensional numpy array
+        @param prediction: a floating point value
+        return value: helper_mean_squared_error_test calculates the mean squared error
         """
-        squared_error_sum  =  float ( 0 )
-        para  etiqueta  en  etiquetas :
-            squared_error_sum  += ( etiqueta  -  predicción ) **  2
+        squared_error_sum = float(0)
+        for label in labels:
+            squared_error_sum += (label - prediction) ** 2
 
-        devolver  flotador ( squared_error_sum  /  etiquetas . tamaño )
+        return float(squared_error_sum / labels.size)
 
 
-def  principal ():
+def main():
     """
-    En esta demostración estamos generando un conjunto de datos de muestra de la función sin en
-    entumecido Luego entrenamos un árbol de decisión en el conjunto de datos y usamos el árbol de decisión para
-    predecir la etiqueta de 10 valores de prueba diferentes. Entonces el error cuadrático medio sobre
-    se muestra esta prueba.
+    In this demonstration we're generating a sample data set from the sin function in
+    numpy.  We then train a decision tree on the data set and use the decision tree to
+    predict the label of 10 different test values. Then the mean squared error over
+    this test is displayed.
     """
-    x  =  np . un rango ( - 1.0 , 1.0 , 0.005 )
-    y  =  np . pecado ( x )
+    x = np.arange(-1.0, 1.0, 0.005)
+    y = np.sin(x)
 
-    árbol  =  DecisionTree ( profundidad = 10 , min_leaf_size = 10 )
-    árbol _ tren ( x , y )
+    tree = DecisionTree(depth=10, min_leaf_size=10)
+    tree.train(x, y)
 
-    test_cases  = ( np . aleatorio . rand ( 10 ) *  2 ) -  1
-    predicciones  =  np . array ([ árbol . predecir ( x ) para  x  en  test_cases ])
-    avg_error  =  np . media (( predicciones  -  test_cases ) **  2 )
+    test_cases = (np.random.rand(10) * 2) - 1
+    predictions = np.array([tree.predict(x) for x in test_cases])
+    avg_error = np.mean((predictions - test_cases) ** 2)
 
-    print ( "Valores de prueba:"  +  str ( test_cases ))
-    print ( "Predicciones: "  +  str ( predicciones ))
-    imprimir ( "Error promedio:"  +  str ( avg_error ))
+    print("Test values: " + str(test_cases))
+    print("Predictions: " + str(predictions))
+    print("Average error: " + str(avg_error))
 
 
-si  __nombre__  ==  "__principal__" :
-    principal ()
-    importar  documento
+if __name__ == "__main__":
+    main()
+    import doctest
 
-    doctest _ testmod ( nombre = "mean_squarred_error" , detallado = True )
+    doctest.testmod(name="mean_squarred_error", verbose=True)
